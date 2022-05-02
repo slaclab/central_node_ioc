322a48b5945e3fffe9ddadf1bc6404da237b7331
Jeremy Mock <jeremy.a.mock@gmail.com>
2022-05-02 12:40:58 -0700

    Get rid of ASG in DEV

--- iocBoot/common/central_node_dev.cmd
+++ iocBoot/common/central_node_dev.cmd
@@ -14,8 +14,8 @@ epicsEnvSet("PHYSICS_TOP", "/afs/slac/g/lcls/physics")
 epicsEnvSet("MPS_ENV_HISTORY_HOST", "lcls-dev3")
 
 # Set ASG groups
-epicsEnvSet("ASG1", "MCC")
-epicsEnvSet("ASG2", "EOICDESK")
+epicsEnvSet("ASG1", "")
+epicsEnvSet("ASG2", "")
 
 # ====================================================================
 # Load the common Central Node startup
