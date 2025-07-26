bfcb0010e40e4e3f03d1641d5bd75a9e4ad9575b
Jeremy Mock <jmock@dev-srv09.slac.stanford.edu>
2025-07-26 16:56:06 -0700

    Change booting for testing

--- iocBoot/common/central_node.cmd
+++ iocBoot/common/central_node.cmd
@@ -156,13 +156,16 @@ dbLoadRecords("db/tprTrig.db", "PORT=${TPRTRIGGER_PORT},LOCA=${LOCATION},IOC_UNI
 dbLoadRecords("db/saveLoadConfig.db", "P=${IOC_PV}, PORT=${CPSW_PORT}")
 
 # Central Node database, from the MPS configuration database
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/device_inputs.db","ASG1=${ASG1},ASG2=${ASG2}")
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/analog_devices.db","ASG1=${ASG1},ASG2=${ASG2}")
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/destinations.db","GLOBAL=${GLOBAL},BASE=${IOC_PV},ASG1=${ASG1},ASG2=${ASG2}")
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/faults.db","ASG1=${ASG1},ASG2=${ASG2}")
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/apps.db","BASE=${IOC_PV},ASG1=${ASG1},ASG2=${ASG2}")
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/conditions.db","BASE=${IOC_PV},ASG1=${ASG1},ASG2=${ASG2}")
-dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/fault_bypass.db","ASG1=${ASG1},ASG2=${ASG2}")
+
+dbLoadRecords(${TOP}/iocBoot/${IOC}/${IOC}.db,"ASG1=${ASG1},ASG2=${ASG2}")
+
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/device_inputs.db","ASG1=${ASG1},ASG2=${ASG2}")
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/analog_devices.db","ASG1=${ASG1},ASG2=${ASG2}")
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/destinations.db","GLOBAL=${GLOBAL},BASE=${IOC_PV},ASG1=${ASG1},ASG2=${ASG2}")
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/faults.db","ASG1=${ASG1},ASG2=${ASG2}")
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/apps.db","BASE=${IOC_PV},ASG1=${ASG1},ASG2=${ASG2}")
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/conditions.db","BASE=${IOC_PV},ASG1=${ASG1},ASG2=${ASG2}")
+#dbLoadRecords("${MPS_ENV_CONFIG_PATH}/central_node_db/cn${CN_INDEX}/fault_bypass.db","ASG1=${ASG1},ASG2=${ASG2}")
 
 # ====================================================================
 # Setup autosave/restore
